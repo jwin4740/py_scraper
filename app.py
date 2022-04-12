54c36ee148c81dc545016753b05a715dccabcbc2
James Winkle <jjwinklejmj@gmail.com>
2022-04-12 13:25:10 -0400

    sql queries

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import sys
 
-from driver import MainDriver
+import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
 from db import start_db
 from dotenv import load_dotenv
 from pathlib import Path
@@ -29,43 +31,64 @@ cursor = Conn.cursor()
 
 
 def insert(record):
-    cursor.execute(
-        "INSERT INTO reports"
-        "(VAERS_ID,REPORT_RECEIVED_DATE,STATE,AGE_YEARS,SEX,SYMPTOM_TEXT,DIED,DATE_DIED,LIFE_THREATENING_ILLNESS,"
-        "ER_VISIT,HOSPITAL_VISIT,DAYS_IN_HOSPITAL,DISABILITY,VACCINATION_DATE,AE_ONSET_DATE,NUM_DAYS_TO_ONSET,"
-        "LAB_DATA,VACCINE_ADMINISTERED_BY,OTHER_MEDICATIONS, CURRENT_ILLNESS, PATIENT_HISTORY,PRIOR_VACCINE,"
-        "VAERS_FORM_NUMBER, "
-        "DATE_FORM_COMPLETED,BIRTH_DEFECT,OFFICE_VISIT,ER_ED_VISIT,ALLERGIES) "
-        " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
-        (record['VAERS_ID'], record['REPORT_RECEIVED_DATE'], record['STATE'], record['AGE_YEARS'], record['SEX'],
-         record['SYMPTOM_TEXT'], record['DIED'], record['DATE_DIED'], record['LIFE_THREATENING_ILLNESS'],
-         record['ER_VISIT'], record['HOSPITAL_VISIT'], record['DAYS_IN_HOSPITAL'], record['DISABILITY'],
-         record['VACCINATION_DATE'], record['AE_ONSET_DATE'], record['NUM_DAYS_TO_ONSET'], record['LAB_DATA'],
-         record['VACCINE_ADMINISTERED_BY'], record['OTHER_MEDICATIONS'], record['CURRENT_ILLNESS'],
-         record['PATIENT_HISTORY'],
-         record['PRIOR_VACCINE'], record['VAERS_FORM_NUMBER'], record['DATE_FORM_COMPLETED'], record['BIRTH_DEFECT'],
-         record['OFFICE_VISIT'], record['ER_ED_VISIT'], record['ALLERGIES']))
+    try:
+        cursor.execute(
+            "INSERT INTO reports"
+            "(VAERS_ID,REPORT_RECEIVED_DATE,STATE,AGE_YEARS,SEX,SYMPTOM_TEXT,DIED,DATE_DIED,LIFE_THREATENING_ILLNESS,"
+            "ER_VISIT,HOSPITAL_VISIT,DAYS_IN_HOSPITAL,DISABILITY,VACCINATION_DATE,AE_ONSET_DATE,NUM_DAYS_TO_ONSET,"
+            "LAB_DATA,VACCINE_ADMINISTERED_BY,OTHER_MEDICATIONS, CURRENT_ILLNESS, PATIENT_HISTORY,PRIOR_VACCINE,"
+            "VAERS_FORM_NUMBER, "
+            "DATE_FORM_COMPLETED,BIRTH_DEFECT,OFFICE_VISIT,ER_ED_VISIT,ALLERGIES) "
+            " VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",
+            (record['VAERS_ID'], record['REPORT_RECEIVED_DATE'], record['STATE'], record['AGE_YEARS'], record['SEX'],
+             record['SYMPTOM_TEXT'], record['DIED'], record['DATE_DIED'], record['LIFE_THREATENING_ILLNESS'],
+             record['ER_VISIT'], record['HOSPITAL_VISIT'], record['DAYS_IN_HOSPITAL'], record['DISABILITY'],
+             record['VACCINATION_DATE'], record['AE_ONSET_DATE'], record['NUM_DAYS_TO_ONSET'], record['LAB_DATA'],
+             record['VACCINE_ADMINISTERED_BY'], record['OTHER_MEDICATIONS'], record['CURRENT_ILLNESS'],
+             record['PATIENT_HISTORY'],
+             record['PRIOR_VACCINE'], record['VAERS_FORM_NUMBER'], record['DATE_FORM_COMPLETED'],
+             record['BIRTH_DEFECT'],
+             record['OFFICE_VISIT'], record['ER_ED_VISIT'], record['ALLERGIES']))
 
-    Conn.commit()
-    print('inserted')
+        Conn.commit()
+        print('inserted record')
+    except Error as E:
+        print(E)
 
-    cursor.close()
-    Conn.close()
+
+def insert_vaccine(record):
+    try:
+        cursor.execute(
+            "INSERT INTO vaccine"
+            "(VAERS_ID,VACCINATION_TYPE,VACCINATION_MANUFACTURER, VACCINATION_LOT, VACCINATION_DOSE_SERIES, "
+            "VACCINATION_ROUTE, VACCINATION_SITE, VACCINATION_NAME) "
+            " VALUES (%s,%s,%s,%s,%s,%s,%s,%s)",
+            (record['VAERS_ID'], record['VACCINATION_TYPE'], record['VACCINATION_MANUFACTURER'],
+             record['VACCINATION_LOT'], record['VACCINATION_DOSE_SERIES'], record['VACCINATION_ROUTE'],
+             record['VACCINATION_SITE'], record['VACCINATION_NAME']))
+
+        Conn.commit()
+        print('inserted record')
+    except Error as E:
+        print(E)
 
 
-def read_csv(year, file_name):
+def read_csv_report(year, file_name):
     file_path = ROOT_PATH / year / file_name
 
     with open(file_path, 'r') as f:
         rows = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL)
         for r in rows:
-            if rows.line_num < 3 and rows.line_num > 1:
+            if rows.line_num > 1:
                 record = {}
 
                 record['VAERS_ID'] = int(r[0])
                 record['REPORT_RECEIVED_DATE'] = datetime.strptime(r[1], "%m/%d/%Y")
                 record['STATE'] = r[2]
-                record['AGE_YEARS'] = float(r[3])
+                if r[3] != '':
+                    record['AGE_YEARS'] = float(r[3])
+                else:
+                    record['AGE_YEARS'] = None
                 record['SEX'] = r[6]
                 record['SYMPTOM_TEXT'] = r[8]
                 if r[9] == 'Y':
@@ -144,7 +167,32 @@ def read_csv(year, file_name):
                     record['ER_ED_VISIT'] = False
                 record['ALLERGIES'] = r[34]
                 insert(record)
+        cursor.close()
+        Conn.close()
+    # lines = f.readlines()
+    # print(lines[1])
 
+
+def read_csv_vaccine(year, file_name):
+    file_path = ROOT_PATH / year / file_name
+
+    with open(file_path, 'r') as f:
+        rows = csv.reader(f, quotechar='"', delimiter=',', quoting=csv.QUOTE_ALL)
+        for r in rows:
+            if rows.line_num > 1:
+                record = {}
+                record['VAERS_ID'] = int(r[0])
+                record['VACCINATION_TYPE'] = r[1]
+                record['VACCINATION_MANUFACTURER'] = r[2]
+                record['VACCINATION_LOT'] = r[3]
+                record['VACCINATION_DOSE_SERIES'] = r[4]
+                record['VACCINATION_ROUTE'] = r[5]
+                record['VACCINATION_SITE'] = r[6]
+                record['VACCINATION_NAME'] = r[7]
+
+                insert_vaccine(record)
+        cursor.close()
+        Conn.close()
     # lines = f.readlines()
     # print(lines[1])
 
@@ -183,7 +231,9 @@ def read_csv(year, file_name):
 #     # insert(record)
 
 
-read_csv(sys.argv[2], sys.argv[3])
+# read_csv_report(sys.argv[2], sys.argv[3])
+
+read_csv_vaccine(sys.argv[2], sys.argv[3])
 # m = '01/02/2020'
 #
 # d = datetime.strptime(m, "%m/%d/%Y")
--- seed.sql
+++ seed.sql
@@ -1,30 +1,45 @@
-CREATE TABLE reports (
-    VAERS_ID int NOT NULL,
-    REPORT_RECEIVED_DATE DATE NOT NULL,
-    STATE VARCHAR(2),
-    AGE_YEARS INT,
-    SEX VARCHAR(1),
-    SYMPTOM_TEXT TEXT,
-    DIED BOOLEAN,
-    DATE_DIED DATE,
-    LIFE_THREATENING_ILLNESS BOOLEAN,
-    ER_VISIT BOOLEAN,
-    HOSPITAL_VIST BOOLEAN,
-    DAYS_IN_HOSPITAL INT,
-    DISABILITY BOOLEAN,
-    VACCINATION_DATE DATE,
-    AE_ONSET_DATE DATE,
-    NUM_DAYS_TO_ONSET INT,
-    LAB_DATA TEXT,
-    VACCINE_ADMINISTERED_BY VARCHAR(3),
-    OTHER_MEDICATIONS TEXT,
-    PATIENT_HISTORY TEXT,
-    PRIOR_VACCINE VARCHAR(300),
-    VAERS_FORM_NUMBER INT,
-    DATE_FORM_COMPLETED DATE,
-    BIRTH_DEFECT BOOLEAN,
-    OFFICE_VISIT BOOLEAN,
-    ER_ED_VISIT BOOLEAN,
-    ALLERGIES TEXT,
-    PRIMARY KEY (vaers_id)
-);
+create database vaers;
+use vaers;
+
+select COUNT(VAERS_ID) from reports;
+
+select COUNT(VAERS_ID) from vaccine;
+
+select * from vaccine v ;
+select * from reports r ;
+
+select r.VAERS_ID, r.AGE_YEARS, r.VACCINATION_DATE, r.DIED, r.DATE_DIED, r.LAB_DATA, r.SYMPTOM_TEXT, r.PATIENT_HISTORY
+from reports r left join vaccine v on r.VAERS_ID = v.VAERS_ID
+where v.VACCINATION_TYPE = 'COVID19' and r.DIED = 1 and r.AGE_YEARS < 50;
+
+-- stomach or nausea
+select r.VAERS_ID, r.AGE_YEARS, r.VACCINATION_DATE, v.VACCINATION_TYPE, r.DIED, r.DATE_DIED, r.SYMPTOM_TEXT, r.LAB_DATA, r.PATIENT_HISTORY
+from reports r left join vaccine v on r.VAERS_ID = v.VAERS_ID
+where v.VACCINATION_TYPE = 'COVID19' and r.DIED = 1 and (r.SYMPTOM_TEXT like '%stomach%' or r.SYMPTOM_TEXT like '%nausea%');
+
+-- suicide
+select r.VAERS_ID, r.AGE_YEARS, r.VACCINATION_DATE, v.VACCINATION_TYPE, r.DIED, r.DATE_DIED, r.SYMPTOM_TEXT, r.LAB_DATA, r.PATIENT_HISTORY
+from reports r left join vaccine v on r.VAERS_ID = v.VAERS_ID
+where v.VACCINATION_TYPE = 'COVID19' and (r.SYMPTOM_TEXT like '%suicide%');
+
+-- neurological
+select r.VAERS_ID, r.AGE_YEARS, r.VACCINATION_DATE, v.VACCINATION_TYPE, r.DIED, r.DATE_DIED, r.SYMPTOM_TEXT, r.LAB_DATA, r.PATIENT_HISTORY
+from reports r left join vaccine v on r.VAERS_ID = v.VAERS_ID
+where v.VACCINATION_TYPE = 'COVID19' and (r.SYMPTOM_TEXT like '%neuro%');
+
+-- crohn's disease OR gerd
+select r.VAERS_ID, r.AGE_YEARS, r.VACCINATION_DATE, v.VACCINATION_TYPE, r.DIED, r.DATE_DIED, r.SYMPTOM_TEXT, r.LAB_DATA, r.PATIENT_HISTORY
+from reports r left join vaccine v on r.VAERS_ID = v.VAERS_ID
+where v.VACCINATION_TYPE = 'COVID19' and r.DIED = 1 and (r.PATIENT_HISTORY like '%crohn%' or r.PATIENT_HISTORY like '%gerd%');
+
+-- guillain barre
+select r.VAERS_ID, r.AGE_YEARS, r.VACCINATION_DATE, v.VACCINATION_TYPE, r.DIED, r.DATE_DIED, r.SYMPTOM_TEXT, r.LAB_DATA, r.PATIENT_HISTORY
+from reports r left join vaccine v on r.VAERS_ID = v.VAERS_ID
+where v.VACCINATION_TYPE = 'COVID19' and (r.SYMPTOM_TEXT like '%barre%');
+
+
+-- creutzfeldt jakob disease
+select r.VAERS_ID, r.AGE_YEARS, r.VACCINATION_DATE, v.VACCINATION_TYPE, r.DIED, r.DATE_DIED, r.SYMPTOM_TEXT, r.LAB_DATA, r.PATIENT_HISTORY
+from reports r left join vaccine v on r.VAERS_ID = v.VAERS_ID
+where v.VACCINATION_TYPE = 'COVID19' and (r.SYMPTOM_TEXT like '%creutz%' or r.SYMPTOM_TEXT like '%cjd%');
+
